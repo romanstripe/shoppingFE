5f7867d53af11309c3e66fce1755d3865bcf79e9
Sey <ppresi14789@gmail.com>
2025-08-02 22:35:33 +0900

    update registration

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -3,6 +3,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { showToastMessage } from "../common/uiSlice";
 import api from "../../utils/api";
 import { initialCart } from "../cart/cartSlice";
+import ToastMessage from "../../common/component/ToastMessage";
 
 export const loginWithEmail = createAsyncThunk(
   "user/loginWithEmail",
@@ -20,7 +21,27 @@ export const registerUser = createAsyncThunk(
   async (
     { email, name, password, navigate },
     { dispatch, rejectWithValue }
-  ) => {}
+  ) => {
+    try {
+      const response = await api.post("/user", { email, name, password });
+      dispatch(
+        showToastMessage({
+          message: "Registration is completed",
+          status: "success",
+        })
+      );
+      navigate("/login");
+      return response.data.data;
+    } catch (error) {
+      dispatch(
+        showToastMessage({
+          message: "Failed to registrate",
+          status: "error",
+        })
+      );
+      return rejectWithValue(error.error);
+    }
+  }
 );
 
 export const loginWithToken = createAsyncThunk(
@@ -43,7 +64,19 @@ const userSlice = createSlice({
       state.registrationError = null;
     },
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(registerUser.fulfilled, (state) => {
+        state.loading = false;
+        state.registrationError = null;
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.registrationError = action.payload;
+      });
+  },
 });
 export const { clearErrors } = userSlice.actions;
 export default userSlice.reducer;
